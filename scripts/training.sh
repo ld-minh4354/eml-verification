ffbcfc2e1e76ef0b79d8a3956ee4d0a0d59e5402
ldminh4354 <ducminh0401@gmail.com>
2025-10-19 16:18:49 -0400

    fix script typo

--- scripts/training.sh
+++ scripts/training.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --gpus-per-node=1
-#SBATCH –-cpus-per-task=1
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=3G
-#SBATCH –-time=01:00:00
+#SBATCH --time=01:00:00
 
 module load python
 srun python baseline_model/training.py
