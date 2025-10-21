1fb1007a0a2ede8b87147459ffa9ba9a69e1cc48
ldminh4354 <ducminh0401@gmail.com>
2025-10-21 17:33:09 -0400

    update training.sh

--- scripts/training.sh
+++ scripts/training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=minhle@gatech.edu
 #SBATCH --mail-type=ALL
 
