915a7f880ff3a6d7979b89ead0b0b03bb4444323
ldminh4354 <ducminh0401@gmail.com>
2025-10-23 14:26:49 -0400

    create test data

--- scripts/train_baseline_model.sh
+++ scripts/train_baseline_model.sh
@@ -2,12 +2,10 @@
 #SBATCH --gpus-per-node=h100:1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=3G
-#SBATCH --time=01:00:00
-#SBATCH --mail-user=minhle@gatech.edu
-#SBATCH --mail-type=ALL
+#SBATCH --time=00:10:00
 
 module load python
 source .venv/bin/activate
 
 SEED=${1:-10}  # Default to 10 if not provided
-srun python baseline_model/training.py --seed $SEED
+srun python code/train_baseline_model.py --seed $SEED
