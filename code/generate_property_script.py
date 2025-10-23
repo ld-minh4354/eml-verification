9eed9e2bed677dcfb72311fab73b8700193d7843
ldminh4354 <ducminh0401@gmail.com>
2025-10-23 15:05:36 -0400

    add cifar10 properties

--- code/generate_property_script.py
+++ code/generate_property_script.py
@@ -1,11 +1,7 @@
 import os, sys
-import random
+import textwrap
 
 import pandas as pd
-import numpy as np
-
-import torch
-from torchvision import datasets, transforms
 
 
 
@@ -22,29 +18,56 @@ class GeneratePropertyScripts:
             sys.path.append(project_path)
 
 
-    def get_file_content(self, index):
-        return f"""
-        import os, sys
-        from dnnv.properties import *
-        import numpy as np
+    def main(self):
+        self.get_labels()
+        self.generate_scripts()
 
-        project_path = os.path.abspath("")
-        if project_path not in sys.path:
-            sys.path.append(project_path)
 
-        N = Network("N")
-        x = Image(os.path.join())
+    def get_labels(self):
+        self.df_labels = pd.read_csv(os.path.join("test_data", self.dataset, "labels.csv"))
 
-        epsilon = Parameter("epsilon", type=float, default=(2.0 / 255))
-        true_class = 7
 
-        Forall(
-            x_,  # x_ is assumed to be normalized, so denormalize before comparing to x
-            Implies(
-                ((x - epsilon) < denormalize(x_) < (x + epsilon)) & (0 < denormalize(x_) < 1),
-                np.argmax(N(x_)) == true_class,
-            ),
-        )
-        """
+    def generate_scripts(self):
+        os.makedirs(os.path.join("properties", self.dataset, f"eps-{self.epsilon}"), exist_ok=True)
+
+        for _, row in self.df_labels.iterrows():
+            index = row["index"]
+            label = row["label"]
+
+            file_path = os.path.join("properties", self.dataset, f"eps-{self.epsilon}", f"property_{index}.py")
+            file_content = self.get_file_content(index, label)
+
+            with open(file_path, "w") as f:
+                f.write(file_content)
+
+
+    def get_file_content(self, index, label):
+        return textwrap.dedent(f"""\
+            import os, sys
+            from dnnv.properties import *
+            import numpy as np
+
+            project_path = os.path.abspath("")
+            if project_path not in sys.path:
+                sys.path.append(project_path)
+
+            N = Network("N")
+            x = Image(os.path.join("test_data", "{self.dataset}", "inputs", "input_{index}.npy"))
+
+            epsilon = Parameter("epsilon", type=float, default={self.epsilon})
+            true_class = {label}
+
+            Forall(
+                x_,  
+                Implies(
+                    ((x - epsilon) < x_ < (x + epsilon)) & (0 < x_ < 1),
+                    np.argmax(N(x_)) == true_class,
+                ),
+            )
+        """)
             
 
+
+if __name__ == "__main__":
+    gps = GeneratePropertyScripts()
+    gps.main()
