fb31eede036fdb5d1cabc21f8c65f61b64e80b12
ldminh4354 <ducminh0401@gmail.com>
2025-11-23 18:40:55 -0500

    add scripts to test model

---
+++ bash_scripts/test_model.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=test_model
+#SBATCH --mem=3G
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0:10:00
+#SBATCH --output=logs/dnnv_reluplex.out
+#SBATCH --error=logs/dnnv_reluplex.err
+
+module load StdEnv/2020
+module load python/3.9
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index -r $HOME/requirements_dnnv.txt
+
+srun python code/test_model.py
