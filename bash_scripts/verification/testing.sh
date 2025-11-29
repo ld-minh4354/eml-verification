de188d0e58b22b0e5c1caaad75c3b923f9d5a179
ldminh4354 <ducminh0401@gmail.com>
2025-11-28 21:01:26 -0500

    update verification scripts

---
+++ bash_scripts/verification/testing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=testing
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=61G
+#SBATCH --time=0:05:00
+#SBATCH --output=logs/abc_testing.out
+
+source .venv/bin/activate
