a9e0ac33a8dc808bd68814df5ba891f0b219d825
ldminh4354 <ducminh0401@gmail.com>
2025-11-29 23:29:04 -0500

    debug script

--- bash_scripts/verification/testing.sh
+++ bash_scripts/verification/testing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=testing
-#SBATCH --gpus-per-node=h100:1
+#SBATCH --gpus-per-node=h100:4
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=61G
 #SBATCH --time=0:05:00
@@ -12,4 +12,4 @@ export OMP_NUM_THREADS=1
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128
 
 srun python $HOME/eml-verification/alpha-beta-CROWN/complete_verifier/abcrown.py \
-    --config $HOME/eml-verification/alpha-beta-CROWN/complete_verifier/exp_configs/tutorial_examples/mnist_cnn_a_adv.yaml
+    --config $HOME/eml-verification/alpha-beta-CROWN/complete_verifier/exp_configs/tutorial_examples/cifar_resnet_2b.yaml
