33d3f34da9bc727d0df813c666e2d1ad5995b18b
ldminh4354 <ducminh0401@gmail.com>
2025-11-29 19:01:30 -0500

    set number of threads in script

--- bash_scripts/verification/testing.sh
+++ bash_scripts/verification/testing.sh
@@ -8,5 +8,7 @@
 
 source $HOME/eml-verification/.venv_abc/bin/activate
 
+export OMP_NUM_THREADS=8
+
 srun python $HOME/eml-verification/alpha-beta-CROWN/complete_verifier/abcrown.py \
     --config $HOME/eml-verification/alpha-beta-CROWN/complete_verifier/exp_configs/tutorial_examples/cifar_resnet_2b.yaml
