6539e0f7aa4f9948d6839670e0dc52073f06a94c
ldminh4354 <ducminh0401@gmail.com>
2025-12-02 12:37:13 -0500

    update testing script

---
+++ bash_scripts/verification/testing.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=verify_MNIST
+#SBATCH --gpus=nvidia_h100_80gb_hbm3_2g.20gb:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=61G
+#SBATCH --time=0:05:00
+#SBATCH --array=0-0
+#SBATCH --output=logs/abc_testing.out
+
+module load StdEnv/2023
+module load python/3.11
+source $HOME/eml-verification/.venv_abc/bin/activate
+
+export OMP_NUM_THREADS=1
+
+for (( X=0; X<20; X++ )); do
+    ID=$((SLURM_ARRAY_TASK_ID * 20 + X))
+    LOGFILE="logs_verification/MNIST_${ID}.out"
+
+    {
+        srun python code/property_gen/generate_property_script.py \
+        --epsilon 0.01 --index $ID --job $SLURM_ARRAY_TASK_ID
+
+        timeout 5m srun python $HOME/eml-verification/alpha-beta-CROWN/complete_verifier/abcrown.py \
+        --config $HOME/eml-verification/properties/current_${SLURM_ARRAY_TASK_ID}.yaml
+
+    } &> "$LOGFILE"
+done
